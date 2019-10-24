eb0e12f2e06befd492e63afdaf2982d694389083
Gaurav <computer12f@gmail.com>
2019-10-24 11:11:46 +0530

    new Changes

--- app.js
+++ app.js
@@ -27,42 +27,42 @@ app.listen(port, function () {
 
 app.get('/', function (req, res) {
   console.log("test")
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/index.html")
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/index.html")
 });
 
 
 app.get('/share/*', function (req, res) {
-  res.redirect("https://play.google.com/store/apps/details?id=com.mike.vostmeproto3");
-  // res.sendFile(__dirname+"/sportsappClone/dhannoo/web/index.html")
+  res.redirect("https://play.google.com/store?hl=en");
+  // res.sendFile(__dirname+"/sportsappClone/dhannoo/web2/index.html")
 });
 
 app.get('/:one/:two',function(req,res){
   console.log(req.params.two)
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two);
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two);
 
 });
 
 app.get('/:one/:two/:three',function(req,res){
   console.log("thrree")
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
-//  console.log(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
+//  console.log(__dirname+"/sportsappClone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
 
 });
 
 app.get('/:one/:two/:three/:four',function(req,res){
   console.log("four")
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four);
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four);
 
 });
 
 app.get('/:one/:two/:three/:four/:five',function(req,res){
   console.log("five")
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five);
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five);
 
 });
 
 app.get('/:one/:two/:three/:four/:five/:six',function(req,res){
   console.log("six")
-  res.sendFile(__dirname+"/sportsappClone/dhannoo/web/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five+"/"+req.params.six);
+  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five+"/"+req.params.six);
 
 });
